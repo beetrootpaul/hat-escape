af10e244afda96451bd53a0a51ef902f79295f13
Beetroot Paul <paul@beetrootpaul.com>
2024-04-02 01:41:53 +0200

    update BeetPx: 0.36.1-fix1 -> 0.37.0-fix1

--- src/beetpx.ts
+++ src/beetpx.ts
@@ -14,42 +14,40 @@ let currentScene: Scene | null = null;
 let nextScene: Scene | null = null;
 
 b_.init({
-  config: {
-    gameCanvasSize: "128x128",
-    fixedTimestep: "60fps",
-    assets: [
-      // IMAGE files
-      g.images.font,
-      g.images.attack,
-      g.images.hero,
-      g.images.light,
-      g.images.tiles,
-      g.images.controls,
-      // MUSIC files
-      g.music.drums1Damped,
-      g.music.bass1Damped,
-      g.music.melody1Damped,
-      g.music.melody2Damped,
-      g.music.drums1,
-      g.music.bass1,
-      g.music.melody1,
-      g.music.melody2,
-      // JSON files
-      g.jsons.font,
-      g.jsons.roomsLdtk,
-    ],
-    debugMode: {
-      available: !window.BEETPX__IS_PROD,
-      fpsDisplay: {
-        enabled: true,
-      },
-    },
-    frameByFrame: {
-      available: !window.BEETPX__IS_PROD,
+  canvasSize: "128x128",
+  fixedTimestep: "60fps",
+  assets: [
+    // IMAGE files
+    g.images.font,
+    g.images.attack,
+    g.images.hero,
+    g.images.light,
+    g.images.tiles,
+    g.images.controls,
+    // MUSIC files
+    g.music.drums1Damped,
+    g.music.bass1Damped,
+    g.music.melody1Damped,
+    g.music.melody2Damped,
+    g.music.drums1,
+    g.music.bass1,
+    g.music.melody1,
+    g.music.melody2,
+    // JSON files
+    g.jsons.font,
+    g.jsons.roomsLdtk,
+  ],
+  debugMode: {
+    available: !window.BEETPX__IS_PROD,
+    fpsDisplay: {
+      enabled: true,
     },
   },
-
-  onStarted() {
+  frameByFrame: {
+    available: !window.BEETPX__IS_PROD,
+  },
+}).then(async ({ startGame }) => {
+  b_.setOnStarted(() => {
     // font
     const fontMetrics = b_.getJsonAsset(g.jsons.font).json;
     const magicBookFont: MagicBookFont = new MagicBookFont(fontMetrics);
@@ -68,9 +66,9 @@ b_.init({
     // scene
     currentScene = new SceneTitleAndControls();
     currentScene.init();
-  },
+  });
 
-  onUpdate() {
+  b_.setOnUpdate(() => {
     if (!pauseMenu?.isActive) {
       nextScene = currentScene?.postUpdate() ?? null;
       if (nextScene) {
@@ -83,10 +81,12 @@ b_.init({
       currentScene?.pauseAnimationsAndTimers();
     }
     pauseMenu?.update();
-  },
+  });
 
-  onDraw() {
+  b_.setOnDraw(() => {
     currentScene?.draw();
     pauseMenu?.draw();
-  },
+  });
+
+  await startGame();
 });
